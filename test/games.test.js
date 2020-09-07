a2e0a9f96d11f15dc0eef167936dd1737b81aa2c
Tijani <tijaniusat4barcelona@gmail.com>
2020-09-07 15:53:00 +0100

    Mocha & Chai installed setup

---
+++ test/games.test.js
@@ -0,0 +1,20 @@
+import server from '../app'
+import { expect } from 'chai'
+import supertest from 'supertest'
+
+const chai = require('chai')
+const should = chai.should()
+
+const request = supertest.agent(server)
+
+describe('Games Api', () => {
+  describe('Index route', () => {
+    it('should return welcome message when / route is matched', (done) => {
+      request.get('/').end((err, res) => {
+        res.status.should.be.equal(200)
+        expect(res.body.message).be.equal('Welcome to Games Api')
+        done()
+      })
+    })
+  })
+}) 
