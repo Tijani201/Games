1001c5d42196364ee6b115b3c256a36318cb1bfb
Tijani <tijaniusat4barcelona@gmail.com>
2020-09-09 12:57:32 +0100

    App.js updated and test file

--- test/games.test.js
+++ test/games.test.js
@@ -1,18 +1,50 @@
 import server from '../app'
 import { expect } from 'chai'
 import supertest from 'supertest'
+import models from '../models'
 
 const chai = require('chai')
 const should = chai.should()
 
 const request = supertest.agent(server)
+const gamesModel = models.Games
 
 describe('Games Api', () => {
+  before(async () => {
+    // create database tables
+    await models.sequelize.sync()
+
+    // add a games to the database
+    await gamesModel.create({
+      title: 'test games',
+      genres: 'comedy',
+      rating: 5,
+      likes: 700,
+      year: 2020,
+      description: 'Just a random test games'
+    })
+  })
+  after(async () => {
+    // empty the database
+    await gamesModel.destroy({ where: {} })
+  })
+
   describe('Index route', () => {
     it('should return welcome message when / route is matched', (done) => {
       request.get('/').end((err, res) => {
         res.status.should.be.equal(200)
         expect(res.body.message).be.equal('Welcome to Games Api')
+        done()
+      })
+    })
+  })
+
+  describe('/GET Get all games', () => {
+    it('it should GET all the games', (done) => {
+      request.get('/games').end((err, res) => {
+        res.status.should.be.equal(200)
+        expect(res.body.games).to.be.an('array')
+        expect(res.body.message).be.equal('Games fetched successfully')
         done()
       })
     })
